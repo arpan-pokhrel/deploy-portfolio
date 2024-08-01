660a9f8b2d99c8392bf36b02620b16021ce35ef5
arpan-pokhrel <iamarpan15@gmail.com>
2024-08-01 22:24:50 +0545

    updated the overall portfolio section

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["cybersecurity", "networking"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
