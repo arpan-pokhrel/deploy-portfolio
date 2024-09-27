f5f9220c60fd1781a0b320c32bc01306cdb3c7bf
arpan-pokhrel <iamarpan15@gmail.com>
2024-09-27 11:02:14 +0545

    design changed

---
+++ assets/js/preloader.js
@@ -0,0 +1,34 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  // /**
+  //  * Preloader
+  //  */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      setTimeout(() => {
+        preloader.classList.add('loaded');
+      }, 1000);
+      setTimeout(() => {
+        preloader.remove();
+      }, 2000);
+    });
+  }
+  /**
+   * Animation on scroll function and init
+   */
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', () => {
+    aos_init();
+  });
+
+});
